693e70e354b29cc42c2560be1732cd6565c7cdd5
Oscar Carlsson <oscca@student.chalmers.se>
2020-11-30 13:31:12 +0100

    Created parser for limit orders

--- data-scraper/fetch_limit_orders_omx.py
+++ data-scraper/fetch_limit_orders_omx.py
@@ -2,7 +2,27 @@ from request_omx import RequestOMX
 import json
 import os
 import time
+import itertools
 from datetime import date
+
+def parse_order(raw_data):
+	timestamp = raw_data['@ts']
+	orders = raw_data['inst']['pd']['pl']
+	bids = []
+	asks = []
+	bids_volume = []
+	asks_volume = []
+
+	for order in orders:
+	    bids.append(order['@b'])
+	    asks.append(order['@a'])
+	    bids_volume.append(order['@bv'])
+	    asks_volume.append(order['@av'])
+
+	combined = list(itertools.chain(bids,asks,bids_volume,asks_volume))
+	return(timestamp +';'+(';'.join(combined)))
+
+csv_header = "sep=;\ntimestamp;bid1;bid2;bid3;bid4;bid5;ask1;ask2;ask3;ask4;ask5;bid_volume1;bid_volume2;bid_volume3;bid_volume4;bid_volume5;ask_volume1;ask_volume2;ask_volume3;ask_volume4;ask_volume5"
 
 today = date.today()
 now = time.localtime()
@@ -25,5 +45,8 @@ for key in stocks_dict:
         data = fetcher.fetch_limitorders(stock["id"])
         if(data.status_code == 200):
             file = open(dir_path+"/"+stock["name"]+".json", "w")
-            file.write(data.text)
+            try:
+                file.write(parse_order(json.loads(data.text)))
+            except:
+                 pass #There were no limit orders or something was malformed, ignore
             #time.sleep(0.5)
