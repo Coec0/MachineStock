276d4353a66294bd486206f1aff47528d068e611
Kevin Rudnick <kevinleerudnick@gmail.com>
2021-02-01 15:20:37 +0100

    Add mysql python connector to docker.

--- stock-predictor/python-docker/test.py
+++ stock-predictor/python-docker/test.py
@@ -2,3 +2,18 @@ print("hejhejhej")
 import torch
 x = torch.rand(5, 3)
 print(x)
+
+import mysql.connector
+
+cnx = mysql.connector.connect(host="host.docker.internal",user="admin", password="kvxxkv11-sql", database="test")     
+
+c = cnx.cursor()
+
+c.execute("SELECT * FROM A")
+
+for x in c:
+	print(x[0])
+
+c.close()
+cnx.close()   
+
