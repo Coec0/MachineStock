76927687d1e5866922876a16c6ea4560591a1832
Oscar Carlsson <oscca@student.chalmers.se>
2021-02-10 10:49:45 +0100

    Create class for adapter. Prints result from class

--- input-adapter/adapter.py
+++ input-adapter/adapter.py
@@ -1,14 +1,30 @@
 import socket
 import struct
+import threading
+import queue
 
-HOST = '127.0.0.1'  # The server's hostname or IP address
-PORT = 2000        # The port used by the server
+class Adapter:
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    while(True):
-        msg_len_raw = s.recv(4) #Read header to get size of message
-        msg_len = struct.unpack('>I', msg_len_raw)[0]
-        print(s.recv(msg_len))
-        print()
+    def __init__(self, host='127.0.0.1', port=2000):
+        self.host = host
+        self.port = port
+        self.queue = queue.Queue()
+        threading.Thread(target=self.__tcp, daemon=True).start() #Start listening
+    
+    def __tcp(self): 
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.connect((self.host, self.port))
+            while(True):
+                msg_len_raw = s.recv(4) #Read header to get size of message
+                msg_len = struct.unpack('>I', msg_len_raw)[0]
+                self.queue.put(s.recv(msg_len).decode())
+
+    #If a timeout is set and a timeout is reached, a "Empty" exception will be raised
+    def get(self, time_out=None):
+        return self.queue.get(timeout=time_out)
+        
+connection = Adapter()
+while(True):
+    print(connection.get())
+
 
