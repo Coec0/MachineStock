9660af963a22574f8a705a262877756a61bf7c61
Kevin Rudnick <kevinleerudnick@gmail.com>
2021-03-15 10:18:44 +0100

    Last week work

--- stock-predictor/python-docker/tmp.py
+++ stock-predictor/python-docker/tmp.py
@@ -1,10 +1,14 @@
 import pandas as pd
 #x_data = pd.read_csv("x.csv", sep=";")
-data = pd.read_csv("x_Nordea_SEB_Swedbank_70_p.csv", sep=";")
+print("Reading csv file...")
+data = pd.read_csv("y_Swedbank_A_1.csv", sep=";", usecols=["600s", "ts"], squeeze=True)
+#data = data.iloc[600:]
+
 
 #x_data = x_data["Timestamp"]
-data = data["ts"]
-data["ts"] = data["ts"].astype(int)
+print("Converting to int...")
+
+#data = data.astype(int)
 #x_data.to_csv("x_ts.csv", index=False, sep = ';')
-data.to_csv("x_ts_Nordea_SEB_Swedbank_70_p.csv", index=False, sep = ';')
+data.to_csv("blaYY.csv", index=False, sep = ';')
 print("Done")
