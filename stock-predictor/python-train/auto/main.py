17852b1e082f1e208a63ace0fa504d47073be82c
Kevin Rudnick <kevinleerudnick@gmail.com>
2021-03-29 16:48:14 +0200

    Change learning rate

--- stock-predictor/python-train/auto/main.py
+++ stock-predictor/python-train/auto/main.py
@@ -20,7 +20,7 @@ fin_inds = ["price", "ema", "rsi", "macd", "volatility", "channels"]
 cols_y = [("5s", 0), ("15s", 2), ("30s", 4), ("60s", 6)]
 epochs = [5, 50]
 batch_sizes = [512]
-learning_rates = [0.01, 0.001, 0.0001]
+learning_rates = [0.01, 0.0001, 0.000001]
 useTime = [False]
 
 iterator = itertools.product(models, epochs, batch_sizes, window_sizes, fin_inds, learning_rates, cols_y, useTime)
