dac69bea5567a9ba1bfd9fb1739b4646db48869e
Kevin Rudnick <kevinleerudnick@gmail.com>
2021-03-29 14:47:56 +0200

    Include time

--- stock-predictor/python-train/auto/main.py
+++ stock-predictor/python-train/auto/main.py
@@ -6,6 +6,7 @@ import os
 import pandas as pd
 import shutil
 from datetime import datetime
+import traceback
 
 # replace % for number
 file_x_const = "Swedbank_A/x_Swedbank_A_%_p_ema_rsi_macd_volatility_channels.csv"
@@ -20,9 +21,9 @@ cols_y = [("5s", 0), ("15s", 2), ("30s", 4), ("60s", 6)]
 epochs = [5, 50]
 batch_sizes = [512]
 learning_rates = [0.01, 0.001, 0.0001]
-useTime = False
+useTime = [False]
 
-iterator = itertools.product(models, epochs, batch_sizes, window_sizes, fin_inds, learning_rates, cols_y)
+iterator = itertools.product(models, epochs, batch_sizes, window_sizes, fin_inds, learning_rates, cols_y, useTime)
 
 resultframe = pd.DataFrame(columns = ["stock", "avg-train-loss", "avg-val-loss", "avg-test-loss", "val-r2", "test-r2", "epochs", "learning-rate", "x-col", "y-col", "window-size", "batch-size", "use-time"])
 
@@ -30,36 +31,37 @@ try:
     for params in iterator:
         print(params)
         cols_x = []
-        model, epoch, batch_size, ws, fin_ind, lr, col_y_tup = params
+        model, epoch, batch_size, ws, fin_ind, lr, col_y_tup, useTime = params
+        mult = 2 if useTime else 1
         if(fin_ind == "price"):
-            input_size = ws
+            input_size = ws*mult
         elif(fin_ind == "ema"):
-            input_size = ws+2
-            cols_x = [ws, ws+1]
+            input_size = ws*mult+2
+            cols_x = [ws*mult, ws*mult+1]
         elif(fin_ind == "rsi"):
-            input_size = ws+1
-            cols_x = [ws+2]
+            input_size = ws*mult+1
+            cols_x = [ws*mult+2]
         elif(fin_ind == "macd"):
-            input_size = ws+1
-            cols_x = [ws+3]
+            input_size = ws*mult+1
+            cols_x = [ws*mult+3]
         elif(fin_ind == "volatility"):
-            input_size = ws+1
-            cols_x = [ws+4]
+            input_size = ws*mult+1
+            cols_x = [ws*mult+4]
         elif(fin_ind == "channels"):
-            input_size = ws+8
-            cols_x = list(range(ws+5, ws+13))
+            input_size = ws*mult+8
+            cols_x = list(range(ws*mult+5, ws*mult+13))
 
         model.instantiate(input_size)
         optimizer = optim.AdamW(model.parameters(), lr=lr, eps=0.001)
 
-        cols_x.extend(list(range(ws)))
+        cols_x.extend(list(range(ws*mult)))
 
         col_y_name, col_y = col_y_tup
 
         files_x = [file_x_const.replace("%", str(ws))]
         files_y = [file_y_const.replace("%", str(ws))]
 
-        foldername = model.getName()+"_"+col_y_name+"_"+str(epoch)+"_"+str(batch_size)+"_"+fin_ind+"_"+str(lr)
+        foldername = model.getName()+"_"+col_y_name+"_"+str(epoch)+"_"+str(batch_size)+"_"+fin_ind+"_"+str(lr)+"_"+str(useTime)
         filepath = "Swedbank_A/"+str(ws)+"/"+foldername+"/"
         if not os.path.exists(filepath):
             os.makedirs(filepath)
@@ -73,6 +75,7 @@ try:
     print("Done")
 except:
     print("Error")
+    traceback.print_exc()
     ts = str(int(datetime.now().timestamp()))
     resultframe.to_csv("Swedbank_A/Swedbank_A_"+ts+".csv", index=False)
     shutil.rmtree(filepath)
