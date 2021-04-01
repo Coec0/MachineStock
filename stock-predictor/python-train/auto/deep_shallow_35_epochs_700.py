8bb6df8a2fc01960470a44071afe3a9265068b9a
Oscar Carlsson <oscca@student.chalmers.se>
2021-04-01 17:32:43 +0200

    Split 700

--- stock-predictor/python-train/auto/deep_shallow_35_epochs_700.py
+++ stock-predictor/python-train/auto/deep_shallow_35_epochs_700.py
@@ -2,16 +2,16 @@ import trainbase
 import itertools
 import main
 
-models = [trainbase.DeepModel()] #trainbase.ShallowModel()
-window_sizes = [70, 200]
+models = [trainbase.DeepModel(), trainbase.ShallowModel()]
+window_sizes = [700]
 fin_inds = ["price", "ema", "rsi", "macd", "volatility", "channels"]
-cols_y = [("5s", 0), ("15s", 2), ("30s", 4), ("60s", 6)]
-epochs = [5]
+cols_y = [("5s", 0), ("30s", 4)]
+epochs = [35]
 batch_sizes = [512]
-learning_rates = [0.01, 0.0001, 0.000001]
+learning_rates = [0.0001, 0.000001]
 useTime = [False]
 normal = [False]
-name = ["standard"]
+name = ["deep_shallow_35_epochs"]
 
 iterator = itertools.product(models, epochs, batch_sizes, window_sizes, fin_inds, learning_rates, cols_y, useTime, normal, name)
 
